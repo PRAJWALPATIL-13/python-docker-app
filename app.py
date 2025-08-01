69a52198ff4a11a29d295923db966654dbd917ab
PrajwalPatil-13 <prajwalpatil128@gmail.com>
2025-08-01 20:02:57 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Dockerized Python App! new commit"
+    return "Hello from Dockerized Python App! new commit 2 test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
